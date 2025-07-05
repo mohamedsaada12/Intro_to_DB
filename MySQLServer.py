1c687f6a87ab1721c54dc3c83467226bcfc79eaa
mohamedsaada12 <profsaada@gmail.com>
2025-07-05 08:38:36 +0300

    Final version for ALX checks

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,13 +1,18 @@
+import os
 import mysql.connector
 from mysql.connector import errorcode
+
+# Read connection parameters from environment, or use defaults
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASS = os.getenv("DB_PASS", "")
 
 try:
     # 1) Establish a connection to the MySQL server
     connection = mysql.connector.connect(
-        host="sql7.freesqldatabase.com",
-        port=3306,
-        user="sql7788081",
-        password="NDZ1llfNGF"
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASS
     )
 
     cursor = connection.cursor()
